c933f639aec4e153a0a939a5a96696ee637ab858
Kousuke-N <kousuke.n2132@gmail.com>
2017-09-13 14:51:36 +0900

    rails_template

---
+++ source.sh
@@ -0,0 +1,3 @@
+alias rails="docker-compose run -e RAILS_ENV=development --rm app bundle exec rails"
+alias rake="docker-compose run -e RAILS_ENV=development --rm app bundle exec rake"
+alias bundle="docker-compose run -e RAILS_ENV=development --rm app bundle"
